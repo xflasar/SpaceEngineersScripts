38f8241ea6b17f2e80352ba3ed1dad1432d91c03
Martin Flasar <christian.flasar@gmail.com>
2024-04-23 13:02:21 +0200

    Added battery management.
    - Battery management for stagger fire mode added.

--- WeaponsManager/RailsManagement.cs
+++ WeaponsManager/RailsManagement.cs
@@ -7,100 +7,13 @@ namespace IngameScript
 {
     public partial class Program : MyGridProgram
     {
-        private bool weaponFiring = false;
-        private double firingDelay = 18;
-        private int currentWeaponIndex = 0;
-        private double countdown = 0;
-        private int weaponTargetFails = 0;
-        private bool weaponsRailsSetToAI = true;
-
-        // Main Method for Firing rail
-        void FireAlternateWeapons()
-        {
-            if (weaponsRailsSetToAI)
-            {
-                weaponsRailsSetToAI = false;
-                weaponTargetFails = 0;
-                WeaponsRails.ForEach(weap =>
-                {
-                    weap.SetValue<Int64>("WC_Shoot Mode", 2);
-                });
-            }
-
-            if (weaponTargetFails > 0) Echo("Weapon Failed to fire: " + weaponTargetFails + "/50");
-
-            Echo("Weapon #: " + currentWeaponIndex + " Cooldown:" + countdown);
-            if (countdown > 0)
-            {
-                countdown -= 1f;
-                return;
-            }
-
-            if (weaponTargetFails > 50)
-            {
-                mode = "AIShoot";
-            }
-
-            if (!WeaponsRails[currentWeaponIndex].GetValueBool("OnOff"))
-            {
-                WeaponsRails[currentWeaponIndex].SetValueBool("OnOff", true);
-                return;
-            }
-
-            if (WeaponsRails[currentWeaponIndex].GetValueBool("WC_FocusFire"))
-            {
-                if (api.GetAiFocus(Me.EntityId) == null)
-                {
-                    currentWeaponIndex = (currentWeaponIndex + 1) % WeaponsRails.Count;
-                    return;
-                }
-            }
-
-            if (!WeaponsRails[currentWeaponIndex].IsFunctional)
-            {
-                currentWeaponIndex = (currentWeaponIndex + 1) % WeaponsRails.Count;
-                return;
-            }
-
-            if (!api.IsWeaponReadyToFire(WeaponsRails[currentWeaponIndex]) || weaponFiring)
-            {
-                currentWeaponIndex = (currentWeaponIndex + 1) % WeaponsRails.Count;
-                return;
-            }
-
-            if (!CanShootTarget(WeaponsRails[currentWeaponIndex]))
-            {
-                currentWeaponIndex = (currentWeaponIndex + 1) % WeaponsRails.Count;
-                return;
-            }
-
-            if (!api.IsTargetAligned(WeaponsRails[currentWeaponIndex],
-                    api.GetWeaponTarget(WeaponsRails[currentWeaponIndex]).Value.EntityId, 0))
-            {
-                currentWeaponIndex = (currentWeaponIndex + 1) % WeaponsRails.Count;
-                return;
-            }
-
-            // Fire the current weapon
-            weaponTargetFails = 0;
-            api.FireWeaponOnce(WeaponsRails[currentWeaponIndex]);
-
-            countdown = CountFiringDelay();
-
-            // Switch to the next weapon
-            currentWeaponIndex = (currentWeaponIndex + 1) % WeaponsRails.Count;
-        }
-
-        double CountFiringDelay()
-        {
-            double distance = Vector3D.Distance(api.GetWeaponTarget(WeaponsRails[currentWeaponIndex]).Value.Position, Me.GetPosition());
-            if (distance < 10000f)
-            {
-                firingDelay = Math.Round(12 + (distance / 10000) * 6);
-            }
-
-            return firingDelay;
-        }
+        private bool _batteryOn = false;
+        private double _countdown = 0;
+        private int _currentWeaponIndex = 0;
+        private double _firingDelay = 18;
+        private bool _weaponFiring = false;
+        private bool _weaponsRailsSetToAI = true;
+        private int _weaponTargetFails = 0;
 
         // Method for checking if we can shoot the target with weapon
         bool CanShootTarget(IMyTerminalBlock weapon)
@@ -113,20 +26,129 @@ namespace IngameScript
             catch (Exception ex)
             {
                 Echo("Error checking if weapon can shoot target: " + ex.Message);
-                weaponTargetFails++;
+                _weaponTargetFails++;
                 return false;
             }
         }
 
+        double CountFiringDelay()
+        {
+            double distance = Vector3D.Distance(api.GetWeaponTarget(WeaponsRails[_currentWeaponIndex]).Value.Position, Me.GetPosition());
+            if (distance < 10000f)
+            {
+                _firingDelay = Math.Round(12 + (distance / 10000) * 6);
+            }
+
+            return _firingDelay;
+        }
+
+        // Main Method for Firing rail
+        void FireAlternateWeapons()
+        {
+            if (_weaponsRailsSetToAI)
+            {
+                _weaponsRailsSetToAI = false;
+                _weaponTargetFails = 0;
+                WeaponsRails.ForEach(weap =>
+                {
+                    weap.SetValue<Int64>("WC_Shoot Mode", 2);
+                });
+            }
+
+            if (_weaponTargetFails > 0)
+            {
+                if (_batteryOn)
+                {
+                    batteries.ForEach(battery =>
+                    {
+                        IMyBatteryBlock bat = battery as IMyBatteryBlock;
+                        bat.ChargeMode = ChargeMode.Auto;
+                    });
+                    _batteryOn = false;
+                }
+                Echo("Weapon Failed to fire: " + _weaponTargetFails + "/50");
+            }
+
+            Echo("Weapon #: " + _currentWeaponIndex + " Cooldown:" + _countdown);
+            if (_countdown > 0)
+            {
+                _countdown -= 1f;
+                return;
+            }
+
+            if (_weaponTargetFails > 50)
+            {
+                mode = "AIShoot";
+            }
+
+            if (!WeaponsRails[_currentWeaponIndex].GetValueBool("OnOff"))
+            {
+                WeaponsRails[_currentWeaponIndex].SetValueBool("OnOff", true);
+                return;
+            }
+
+            if (WeaponsRails[_currentWeaponIndex].GetValueBool("WC_FocusFire"))
+            {
+                if (api.GetAiFocus(Me.EntityId) == null)
+                {
+                    _currentWeaponIndex = (_currentWeaponIndex + 1) % WeaponsRails.Count;
+                    return;
+                }
+            }
+
+            if (!WeaponsRails[_currentWeaponIndex].IsFunctional)
+            {
+                _currentWeaponIndex = (_currentWeaponIndex + 1) % WeaponsRails.Count;
+                return;
+            }
+
+            if (!api.IsWeaponReadyToFire(WeaponsRails[_currentWeaponIndex]) || _weaponFiring)
+            {
+                _currentWeaponIndex = (_currentWeaponIndex + 1) % WeaponsRails.Count;
+                return;
+            }
+
+            if (!CanShootTarget(WeaponsRails[_currentWeaponIndex]))
+            {
+                _currentWeaponIndex = (_currentWeaponIndex + 1) % WeaponsRails.Count;
+                return;
+            }
+
+            if (!api.IsTargetAligned(WeaponsRails[_currentWeaponIndex],
+                    api.GetWeaponTarget(WeaponsRails[_currentWeaponIndex]).Value.EntityId, 0))
+            {
+                _currentWeaponIndex = (_currentWeaponIndex + 1) % WeaponsRails.Count;
+                return;
+            }
+
+            if (!_batteryOn)
+            {
+                batteries.ForEach(battery =>
+                {
+                    IMyBatteryBlock bat = battery as IMyBatteryBlock;
+                    bat.ChargeMode = ChargeMode.Discharge;
+                });
+            }
+
+            // Fire the current weapon
+            _weaponTargetFails = 0;
+            api.FireWeaponOnce(WeaponsRails[_currentWeaponIndex]);
+
+            _countdown = CountFiringDelay();
+
+            // Switch to the next weapon
+            _currentWeaponIndex = (_currentWeaponIndex + 1) % WeaponsRails.Count;
+        }
+
         void SetRailsAutofire()
         {
-            if(weaponsRailsSetToAI) return;
+            if (_weaponsRailsSetToAI) return;
 
             WeaponsRails.ForEach(w =>
             {
                 w.SetValue<Int64>("WC_Shoot Mode", 0);
             });
-            weaponsRailsSetToAI = true;
+            _weaponsRailsSetToAI = true;
         }
     }
 }
